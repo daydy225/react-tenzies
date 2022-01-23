ff91bad276590d3a9a7cc241d51a9dc109bc8148
daydy225 <dabelydadi@mail.com>
2022-01-23 02:00:16 +0000

    Tenzies game update
    UI generate and hold dice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,88 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { nanoid } from 'nanoid';
 import Die from './Die'
 import './reset.css'
 import './styles.css'
 
-export default function App() {
+export default function App() {  
+    
+    const [dice, setDice] = useState(allNewDice())
+
+    // Tenzies state correspont au moment le joueur terniner le jeu.
+    const [tenzies, setTenzies] = useState(false)
+
+useEffect(()=> {
+    //  Regarde a chaque changement d'etat des des
+    console.log("dice state changed")
+},[dice])   
+     
+    
+    function generateNewDice() {
+        // Objet contenant id, value(nbr aleatoire de 1 a 6), et le changement d'etat des de(vert=true, blanc=false)
+        const newDiceObj = {
+            id: nanoid(),
+            value: Math.ceil(Math.random() * 6),
+            isheld: false
+        }
+      return newDiceObj
+    }
+  
+//   console.log(allNewDice())
+
+    function allNewDice() {
+    //  creer une liste de 10 nombres aleatoire de 1 a 6
+   const newDice = []
+   for(let i=0; i < 10; i++) {
+     newDice.push(generateNewDice())
+   }
+   return newDice
+}
+
+// Tous les des seront rouler sauf les des maintenu en veryt
+function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+        return die.isheld ? die : generateNewDice()
+    }))
+}
+ 
+//Les des dont les numeros seront maintenus s'afficheront en vert
+function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+       return die.id === id ? {...die , isheld: !die.isheld} : die
+    }))
+}
+
+
+
+     const diceElements = dice.map(die => (
+     <Die 
+     key={die.id}
+     value={die.value}
+     isheld={die.isheld}
+     holdDice={()=> holdDice(die.id)} 
+     />
+     ))
+   
+
+
+
     return (
         <main>
             <div>
             <h1 className="tenzies--title">Tenzies</h1>
             <p className="tenzies-desc">
-            Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+            Roll until all dice are the same. Click <br/>each die to freeze it at its current value between rolls.
             </p>
             </div>
 
             <div className="dices">
-                <Die/>
+                {diceElements}
             </div>
+            <button className="roll-dice"
+            onClick={rollDice}
+            >
+                Roll
+            </button>
         </main>
     )
 }
--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 
-export default function Die() {
-    return (<div 
-    className="die"
+export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isheld ? "#59E391" : "#fff"
+    } 
+
+    return (
+    <div 
+    className="die-face"
+    onClick={props.holdDice}
+    style={styles}
     >
-        <h3 className="die-num">1</h3>
+        <h3 className="die-num">
+            {props.value}
+        </h3>
     </div>)
 }
